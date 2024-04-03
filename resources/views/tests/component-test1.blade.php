7de0a2e560908131efc13974ad3a3c0948ae9933
takahashilaravel <yu-takahashi@wiz-tech.jp>
2024-04-03 13:13:15 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,6 +1,8 @@
 <x-tests.app>
   <x-slot name="header">ヘッダー1</x-slot>
 コンポーネントテスト1
+
   <x-tests.card title="タイトル1" content="本文1" :cmessage="$message" />
   <x-tests.card title="タイトル2" />
+  <x-tests.card title="CSSを変更したい" class="bg-red-300" />
 </x-tests.app>
