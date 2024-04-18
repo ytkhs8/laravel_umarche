23762f2a6266dccc4533cc16e66ef4c7ba0ecf5b
takahashilaravel <yu-takahashi@wiz-tech.jp>
2024-04-18 17:15:08 +0900

    Store

--- resources/views/layouts/owner-navigation.blade.php
+++ resources/views/layouts/owner-navigation.blade.php
@@ -4,10 +4,12 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="flex-shrink-0 flex items-center">
+                    <div class="w-12">
                     <a href="{{ route('owner.dashboard') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
+                    </div>
                 </div>
 
                 <!-- Navigation Links -->
