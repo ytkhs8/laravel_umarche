79fbc2ad7d2d4d1d326ebcdbae4f064d2d9f9473
takahashilaravel <yu-takahashi@wiz-tech.jp>
2024-05-02 19:03:08 +0900

    リサイズなしアップロード

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Models\Owner;
 use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ShopController extends Controller
 {
@@ -42,10 +44,18 @@ class ShopController extends Controller
 
     public function edit($id)
     {
-        dd(Shop::findOrFail($id));
+        $shop = Shop::findOrFail($id);
+        // dd(Shop::findOrFail($id));
+        return view('owner.shops.edit', compact('shop'));
     }
 
     public function update(Request $request, $id)
     {
+        $imageFile = $request->image; //一時保存
+        if(!is_null($imageFile) && $imageFile->isValid() ){
+        Storage::putFile('public/shops', $imageFile);
+        }
+
+        return redirect()->route('owner.shops.index');
     }
 }
--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -9,15 +9,15 @@
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 bg-white border-b border-gray-200">
-                  @foreach ($shops as $shop)
+                  @foreach ($shops as $shop )
                         <div class="w-1/2 p-4">
-                        <a href="{{ route('owner.shops.edit', ['shop' => $shop->id]) }}">
+                        <a href="{{ route('owner.shops.edit', ['shop' => $shop->id ])}}">
                         <div class="border rounded-md p-4">
                             <div class="mb-4">
                             @if ($shop->is_selling)
                               <span class="border p-2 rounded-md bg-blue-400 text-white">販売中</span>
                             @else
-                            <span class="border p-2 rounded-md bg-red-400 text-white">停止中</span>
+                              <span class="border p-2 rounded-md bg-red-400 text-white">停止中</span>
                             @endif
                             </div>
                             <div class="text-xl">{{ $shop->name }}</div>
