6449dc1165a4fcd4c86f016c9fdf63040cb2da27
takahashilaravel <yu-takahashi@wiz-tech.jp>
2024-05-08 18:36:31 +0900

    image初期設定

--- routes/owner.php
+++ routes/owner.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ImageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -33,6 +34,8 @@ Route::prefix('shops')->
         Route::post('update/{shop}', [ShopController::class, 'update'])->name('shops.update');
 });
 
+Route::resource('images', ImageController::class)
+->middleware('auth:owners')->except(['show']);
 
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
