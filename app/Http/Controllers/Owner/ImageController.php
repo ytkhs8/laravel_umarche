896ff2f7d414123fae5a4ffc0801bf71407d5552
takahashilaravel <yu-takahashi@wiz-tech.jp>
2024-05-16 22:11:18 +0900

    product index, eager loading

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -10,6 +10,7 @@ use App\Http\Requests\UploadImageRequest;
 use App\Services\ImageService;
 use Illuminate\Support\Facades\Storage;
 
+
 class ImageController extends Controller
 {
     public function __construct()
