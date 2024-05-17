e039706c7ceeec08476d72941d6e22f1a5070ddd
takahashilaravel <yu-takahashi@wiz-tech.jp>
2024-05-17 16:09:43 +0900

    category

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -7,7 +7,8 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Models\Image;
 use Illuminate\Support\Facades\Auth;
-use App\Models\SecondaryCategory;
+use App\Models\Shop;
+use App\Models\PrimaryCategory;
 use App\Models\Owner;
 
 class ProductController extends Controller
@@ -57,7 +58,21 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $shops = Shop::where('owner_id', Auth::id())
+        ->select('id', 'name')
+        ->get();
+
+        $images = Image::where('owner_id', Auth::id())
+        ->select('id', 'title', 'filename')
+        ->orderBy('updated_at', 'desc')
+        ->get();
+
+        $categories = PrimaryCategory::with('secondary')
+        ->get();
+
+        return view('owner.products.create', 
+            compact('shops', 'images', 'categories'));
+
     }
 
     /**
