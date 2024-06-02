650f2e6a3aef436180c7816a8ccd35f9729796c7
takahashilaravel <yu-takahashi@wiz-tech.jp>
2024-06-02 22:48:36 +0900

    product destroy

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -213,6 +213,11 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Product::findOrFail($id)->delete();
+
+        return redirect()
+        ->route('owner.products.index')
+        ->with(['message' => '商品を削除しました。',
+        'status' => 'alert']);
     }
 }
