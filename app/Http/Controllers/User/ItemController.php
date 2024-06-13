8a83ab3e894b1bda7ca44eb66b3c4b99ef1e897c
takahashilaravel <yu-takahashi@wiz-tech.jp>
2024-06-13 14:55:42 +0900

    show調整

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:users');
+    }
     public function index()
     {
         $stocks = DB::table('t_stocks')
@@ -41,4 +45,12 @@ class ItemController extends Controller
 
         return view('user.index', compact('products'));
     }
+
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+
+        return view('user.show', compact('product'));
+    }
+
 }
