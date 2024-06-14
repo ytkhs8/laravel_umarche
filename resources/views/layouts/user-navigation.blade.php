50754122c5be087a89b8c8daf9fde5fb7cd1df39
takahashilaravel <yu-takahashi@wiz-tech.jp>
2024-06-14 15:24:14 +0900

    swiper

--- resources/views/layouts/user-navigation.blade.php
+++ resources/views/layouts/user-navigation.blade.php
@@ -4,7 +4,7 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="flex-shrink-0 flex items-center">
                     <div class="w-12">
                     <a href="{{ route('user.items.index') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
