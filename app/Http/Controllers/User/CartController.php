c21a3dd2a93a8cae136fd4cb02a93f4a434e2496
takahashilaravel <yu-takahashi@wiz-tech.jp>
2024-06-21 15:43:17 +0900

    stripeキャンセル処理

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -102,7 +102,7 @@ class CartController extends Controller
             'line_items' => [[$lineItems]],
             'mode' => 'payment',
             'success_url' => route('user.cart.success'),
-            'cancel_url' => route('user.cart.index'),
+            'cancel_url' => route('user.cart.cancel'),
 
         ]);
 
@@ -116,5 +116,20 @@ class CartController extends Controller
         Cart::where('user_id', Auth::id())->delete();
 
         return redirect()->route('user.items.index');
+    }
+
+    public function cancel()
+    {
+        $user = User::findOrFail(Auth::id());
+
+        foreach($user->products as $product){
+            Stock::create([
+                'product_id' => $product->id,
+                'type' => \Constant::PRODUCT_LIST['add'],
+                'quantity' => $product->pivot->quantity
+            ]);
+        }
+
+        return redirect()->route('user.cart.index');
     }
 }
